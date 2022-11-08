42859f57535664278ef8352f7c3acb38ed6a95ba
Ahmed Ibrahim <ib153507@gmail.com>
2022-11-08 17:36:05 +0200

    Setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import { Message, MessageType } from "./types";
 import NewShortcutForm from "./components/NewShortcutForm";
 import "./index.css";
-import storage from "./chrome/storage";
 
 function App() {
-  useEffect(() => {
-    chrome.tabs &&
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        async (tabs) => {
-          // console.log({ tabs });
-          // chrome.tabs.sendMessage(tabs[0].id || 0, {
-          //   type: MessageType.ShortcutsList,
-          //   shortcuts: await storage.getShortcuts(),
-          // } as Message);
-        }
-      );
-  }, []);
-
   return (
     <div>
       <Navbar />
