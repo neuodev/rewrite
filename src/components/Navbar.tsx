71a1dd2161ea1d341d746e11cae60dada14b9b07
Ahmed Ibrahim <ib153507@gmail.com>
2022-11-08 17:41:05 +0200

    Add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar: React.FC<{}> = () => {
   return (
-    <div>
-      <h1>Shortcuts</h1>
+    <div className="flex items-center justify-center h-20 bg-gray-100">
+      <img
+        src="/logo192.png"
+        alt="logo"
+        title="Rewrite"
+        className="inline-block mr-4 w-8 h-8 overflow-hidden object-contain"
+      />
+      <h1 className="text-2xl font-bold">Rewrite</h1>
     </div>
   );
 };
